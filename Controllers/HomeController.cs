73ae3aeedb537e9054539769c2b37a2aad4f92b3
irfaniskandar-23 <irfaniskandar791@gmail.com>
2026-02-03 00:01:14 +0800

    Add HomeController with root welcome endpoint

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dev_api.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private static readonly string[] value = ["/api/student"];
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                message = "Welcome to fun-demo API!",
+                availableEndpoints = value
+            });
+        }
+    }
+}
